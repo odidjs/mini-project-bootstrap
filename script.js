e707734409a7fcddbdff803f6d1e15cdc670374b
odidjs <odidewangga3@gmail.com>
2021-12-05 12:23:57 +0700

    menambahkan-DOM

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-let scrGame = document.getElementsByClassName("featured-game");
+//Form Feedback notif dan clear content
+const forms = document.querySelector(".form-email");
+const forms2 = document.querySelector(".form-feedback");
+const feeds = document.querySelector(".feedback");
 
-function notif() {
-  alert("hallo");
+feeds.addEventListener("click", function () {
+  forms.value = "";
+  forms2.value = "";
+  alert("Feedback Terkirim");
+});
+
+//windows scroll
+window.addEventListener("scroll", function () {
+  let navs = document.querySelector(".navbar");
+  let navi = document.querySelector(".brand1");
+  navs.classList.toggle("sticky", window.scrollY > 0);
+  navi.classList.toggle("brand-none", window.scrollY > 0);
+});
+
+//Dark Mode
+function dark() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
 }
